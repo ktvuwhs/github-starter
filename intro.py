a210d22363d9358721de277cc57bc3aec8fe6592
Kyle Vu <ktvu@hbuhsd.edu>
2026-09-01 18:28:08 -0700

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,8 @@
+def yah_yeet(name, message):
+  print(f'Hello {name}!  You have 1 unread message')
+  print(message)
+
+
+msg = input('What is your name? ')
+user_name = input('What would you like to say? ')
+yah_yeet(user_name, msg)
