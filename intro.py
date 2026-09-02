79566cf14d0d2910902ee1c49cb05ae7d4365067
Kyle Vu <ktvu@hbuhsd.edu>
2026-09-01 18:35:17 -0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -6,3 +6,25 @@ def yah_yeet(name, message):
 msg = input('What is your name? ')
 user_name = input('What would you like to say? ')
 yah_yeet(user_name, msg)
+
+
+""" Comments Section for Responses
+1. Predict what will happen when you run the program.  Provide a sample input and predict the output.
+
+
+
+
+2. What is the actual output when running the program (provide your input)?
+
+
+
+
+
+3. Why does this occur in this way?
+
+
+
+
+4. How would you fix this issue?
+
+"""
